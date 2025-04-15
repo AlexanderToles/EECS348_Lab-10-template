5f17cf8725c6d4055556bc19e8969db0abad1f6c
AlexanderToles <alexandertoleseducation@gmail.com>
2025-04-15 14:21:24 -0500

    test

--- calculator.cpp
+++ calculator.cpp
@@ -1,9 +1,11 @@
 #include "calculator.hpp"
 #include <string>
+#include <cmath>
 using namespace std;
 
 double parse_number(const string &expression){
     string fixed_str = expression;
+    double parsed_num = 0.0;
     bool decimal_flag = 0;
     bool operator_flag = 0;
     if((fixed_str[0] == '+')||( fixed_str[0] == '-')){
@@ -39,5 +41,15 @@ double parse_number(const string &expression){
             fixed_str.erase(fixed_str.begin(),fixed_str.begin()+1);
         }
     }
-    return(stod(expression));
+    if(fixed_str[0]=='-'){
+        size_t found = fixed_str.find_first_of("."); 
+        for(int i = found;i > 0;--i){
+            parsed_num+=fixed_str[i]*i;
+        }
+        for(int i = fixed_str.length();i > found;--i){
+            parsed_num+=fixed_str[i]*pow(0.1,i);
+        }
+    }
+    
+    return(parsed_num);
 }
